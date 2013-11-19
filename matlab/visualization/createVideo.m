4dcb96107dde9fb4f9a41e62b91f44f898b64ee0
Daniel Pickem <daniel.pickem@gatech.edu>
2013-11-18 21:11:58 -0500

    Updated visualization tools to produce videos

--- matlab/visualization/createVideo.m
+++ matlab/visualization/createVideo.m
@@ -8,35 +8,58 @@
 % filename.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [] = createVideo(edges, filename, k)
-
-  % Visualize each edge addition and save it as an image
-  h = figure(1);
-  set(gcf, 'Position', get(0,'Screensize')); 
-  counter = 1;
-  for i = 1 : size(edges,1)
-
-    % Remove the red drawing and draw blue again
-    if(i > 1)
-      delete(oldh);
-      plot3(edges(i-1,[1,4]), edges(i-1,[2,5]), edges(i-1,[3,6]), 'o-'); hold on;
+function [] = createVideo(edges, filename, k, edges2)
+    drawGrid = false;
+    
+    % Visualize each edge addition and save it as an image
+    h = figure(1);
+    if(drawGrid) grid on; end
+    set(gcf, 'Position', get(0,'Screensize'));
+    counter = 1;
+    
+    % Check if second tree was provided
+    if(nargin < 4)
+        edges2 = [];
     end
     
-    % Draw the latest edge in red
-    oldh = plot3(edges(i,[1,4]), edges(i,[2,5]), edges(i,[3,6]), 'ro-'); hold on;
-    
-    % Save the figure as an image while changing the view
-    if(mod(i,k) == 0)
-      name = sprintf('im%05d',counter);
-      view(counter*3, 28);
-      axis([-3.14 3.14 -3.14 3.14 -3.14 3.14]);
-      axis square;    
-      print(h,name,'-dpng');
-      counter = counter+1;
+    for i = 1 : max(size(edges,1), size(edges2,1))
+        % Remove the red drawing and draw blue again
+        if(i > 1)
+            if(i < size(edges,1))
+                delete(oldh1);
+                plot3(edges(i-1,[1,4]), edges(i-1,[2,5]), edges(i-1,[3,6]), 'o-'); hold on;
+            end
+            
+            if(i < size(edges2,1))
+                delete(oldh2);
+                plot3(edges2(i-1,[1,4]), edges2(i-1,[2,5]), edges2(i-1,[3,6]), 'go-'); hold on;
+            end
+            if(drawGrid) grid on; end
+        end
+        
+        % Draw the latest edge of tree1 and tree2 in red
+        if(i < size(edges,1))
+            oldh1 = plot3(edges(i,[1,4]), edges(i,[2,5]), edges(i,[3,6]), 'ro-'); hold on;
+        end
+        
+        if(i < size(edges2,1))
+            oldh2 = plot3(edges2(i,[1,4]), edges2(i,[2,5]), edges2(i,[3,6]), 'ro-'); hold on;
+        end
+            
+        if(drawGrid) grid on; end
+        
+        % Save the figure as an image while changing the view
+        if(mod(i,k) == 0)
+            name = sprintf('im%05d',counter);
+            view(counter*3, 28);
+            axis([-3.14 3.14 -3.14 3.14 -3.14 3.14]);
+            axis square;
+            print(h,name,'-dpng');
+            counter = counter+1;
+        end
     end
-  end
-  
-  % Create a video out of the images
-  system(['ffmpeg -i im%05d.png -y -r 180 -vb 15M -vcodec libx264 ', filename, '.mp4']);
-  system('rm -rf im*');
+    
+    % Create a video out of the images
+    %system(['ffmpeg -i im%05d.png -y -r 180 -vb 15M -vcodec libx264 ', filename, '.mp4']);
+    %system('rm -rf im*');
 end
--- matlab/visualization/drawScene.m
+++ matlab/visualization/drawScene.m
@@ -6,18 +6,35 @@
 % dimensions (w,h) from the given file, and the arm.
 % ==================================================================================================
 
-function [] = drawScene (file, q, ql, q_init, q_goal) 
-  data = load(file);
-  for i = 1 : size(data,1)
-    drawRectangle(data(i, 1:3), data(i, 4:5));
-  end
-  
-  hold on;
-  if(nargin > 3)
-      drawArm3(q, ql, 0.1, file, q_init, q_goal)
-  else
-      drawArm3(q, ql, 0.1, file)
-  end
-  set(gcf, 'Position', get(0,'Screensize')); 
-  axis equal;
+function [] = drawScene (file, q, ql, videoFilename, q_init, q_goal) 
+    % Check if video should be created
+    if(~isempty(videoFilename))
+        video = true;
+        
+        % Delete all previous images
+        system('rm -rf im*');
+    else
+        video = false;
+    end
+    
+    data = load(file);
+    for i = 1 : size(data,1)
+        drawRectangle(data(i, 1:3), data(i, 4:5));
+    end
+
+    hold on;
+    if(nargin > 3)
+        drawArm3(q, ql, 0.1, video, file, q_init, q_goal)
+    else
+        drawArm3(q, ql, 0.1, video, file)
+    end
+    
+    % create video
+    if(video)
+        %system(['ffmpeg -i im%05d.png -y -r 180 -vb 15M -vcodec libx264 ', videoFilename, '.mp4']);
+        %system('rm -rf im*');
+    end
+    
+    set(gcf, 'Position', get(0,'Screensize'));
+    axis equal;
 end
