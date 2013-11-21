de2daed3dff5968eabcc1bdab41329c7248b2f63
Daniel Pickem <daniel.pickem@gatech.edu>
2013-11-21 13:25:13 -0500

    Updated problem 3

--- matlab/problem_3.m
+++ matlab/problem_3.m
@@ -10,8 +10,8 @@ addpath('./visualization');
 % rrt_variant     = 'baseline'; 
 rrt_variant     = 'goal_directed';
 % rrt_variant     = 'goal_connect';
-numberOfSamples = 200;
-stepSize        = 0.05;
+numberOfSamples = 500;
+stepSize        = 0.01;
 goalProbability = 0.02;
 errorThreshold  = 0.1;
 epsilon = 0.001;
@@ -39,17 +39,14 @@ X = [];
 % Start timing
 tic
 
-% Set flag for 2c connect variant of RRT goal-directed
-connect = false;
-
-while(length(G) < numberOfSamples)
+while(size(G,2) < numberOfSamples)
     % Sample random configuration
 
     % move towards goal with probability goalProbability
-    if(rand() > goalProbability || connect)
+    if(rand() < goalProbability)
         q_rand = q_goal;
     else
-        q_rand = [rand(); rand(); rand()] * 2 * pi- pi; 
+        q_rand = [rand(); rand(); rand()] * 2 * pi- pi;
     end
 
     % Find nearest point in already grown tree
@@ -72,49 +69,65 @@ while(length(G) < numberOfSamples)
     % Compute the error given the following constraint
     dy      = norm(3 - x(2));
     dy_prev = inf;
-    q_new = [];
 
     % If there is non-zero error, drive q_near closer to the constraint
     if(dy > 0)
         for i = 1:1:steps
+            % Recompute end effector position
             x = fk(q_step, ql)';            
             
             % Recompute current error
             dx      = 0;
             dy      = norm(3 - x(2));
             dtheta  = 0;
+            dError  = [dx; dy; dtheta];
+            
             % Recompute velocities
-            vx = dx / steps;
+            vx = 0;
             vy = dy / steps;
-            w  = dtheta / steps;
+            w  = 0;
             xDot    = [vx; vy; w];
-            xDot = 0.05 * xDot / norm(xDot);
+            xDot = stepSize * xDot / norm(xDot);
             
+            % If the motion does not get us closer to the constraint than
+            % the previous step, break
             if(dy >= dy_prev)
+                disp('Break because dy > dy_prev');
                 break;
+            else
+                % else update the previous error to the current error
+                dy_prev = dy;
             end
-
-            dy_prev = dy;
             
+            % If the next step is closer than epsilon to the constraint,
+            % break
            	if(dy < epsilon)
+                disp('Break because dy < epsilon');
                 break
-            end            
+            end        
+            
             q_new = q_step;
             X = [X, x];
 
-            % Compute joint velocities based on linear end effector velocities
-            if(rank(J) < 3)
-                disp('J is singular');
-            end
-
             % Recompute Jacobian
             J = armJacobian(q_step, ql);
-            qDot = inv(J) * xDot
+            
+            % Compute joint velocities based on linear end effector velocities
+            if(rank(J) < 3)
+                error('J is singular');
+            end
+
+            % Update joint velocities
+            qDot = inv(J) * xDot;
 
             % Recompute joint angles with inverse Jacobian keeping in mind that
             % xDot = J * qDot --> qDot = inv(J) * xDot
             q_step = q_step + qDot;
+            
             if(norm(q_step - q_new) > 0.5)
+                % Don't wanna get away to far from inital expanded state
+                % otherwise the Jacobian is not valid anymore.
+                disp('Break because norm(q_step - q_new) > 0.5');
                 break;
             end
 
@@ -125,6 +138,9 @@ while(length(G) < numberOfSamples)
     collision = 0;
     if(~collision)
         G = [G, q_new];
+        
+        x_added = fk(q_new, ql)'
+        
         E(length(E) + 1) = index;       % store index of q_near as parent
         
         if(~mod(length(G), 10))
@@ -136,29 +152,74 @@ while(length(G) < numberOfSamples)
         disp(['Goal found']);
         break;
     end
+    
+    % Plot data in 3D space
+    plot3(G(1,:), G(2,:), G(3,:), 'bx');
+    grid on; hold on;
+    
+    % Mark initial configuration
+    plot3(G(1,1), G(2,1), G(3,1), 'ro',...
+        'LineWidth',2,...
+        'MarkerEdgeColor','b',...
+        'MarkerFaceColor','r',...
+        'MarkerSize',12)
+    
+    % Mark goal configuration in case of goal directed RRTs
+    if(strcmp(rrt_variant, 'goal_directed'))
+        plot3(q_goal(1), q_goal(2), q_goal(3), 'go',...
+            'LineWidth',2,...
+            'MarkerEdgeColor','b',...
+            'MarkerFaceColor','g',...
+            'MarkerSize',12)
+    end
+    
+    % Draw random sampled config and taken step
+    h_old = plot3(q_rand(1), q_rand(2), q_rand(3), 'bo',...
+        'LineWidth',2,...
+        'MarkerEdgeColor','b',...
+        'MarkerFaceColor','b',...
+        'MarkerSize',12); hold on;
+    
+    % Plot edges of tree
+    for i = 1:1:length(E)
+        Xedge = [G(1, i), G(1, E(i))];
+        Yedge = [G(2, i), G(2, E(i))];
+        Zedge = [G(3, i), G(3, E(i))];
+        line(Xedge, Yedge, Zedge);
+    end
+    
+    % Add labels to axes
+    xlabel('Joint angle 1 q_1','fontsize',14,'fontweight','b')
+    ylabel('Joint angle 2 q_2','fontsize',14,'fontweight','b')
+    zlabel('Joint angle 3 q_3','fontsize',14,'fontweight','b')
+    axis([-pi pi -pi pi -pi pi]);
+    pause(0.1);
+    
+    delete(h_old);
 end
 
 % End timing
 toc
 
+%% Draw tree
 % Plot data in 3D space
 plot3(G(1,:), G(2,:), G(3,:), 'bx');
 grid on; hold on;
 
 % Mark initial configuration
 plot3(G(1,1), G(2,1), G(3,1), 'ro',...
-                'LineWidth',2,...
-                'MarkerEdgeColor','b',...
-                'MarkerFaceColor','r',...
-                'MarkerSize',12)
-            
+    'LineWidth',2,...
+    'MarkerEdgeColor','b',...
+    'MarkerFaceColor','r',...
+    'MarkerSize',12)
+
 % Mark goal configuration in case of goal directed RRTs
 if(strcmp(rrt_variant, 'goal_directed'))
     plot3(q_goal(1), q_goal(2), q_goal(3), 'go',...
-                'LineWidth',2,...
-                'MarkerEdgeColor','b',...
-                'MarkerFaceColor','g',...
-                'MarkerSize',12)
+        'LineWidth',2,...
+        'MarkerEdgeColor','b',...
+        'MarkerFaceColor','g',...
+        'MarkerSize',12)
 end
 
 % Plot edges of tree
@@ -173,20 +234,20 @@ end
 xlabel('Joint angle 1 q_1','fontsize',14,'fontweight','b')
 ylabel('Joint angle 2 q_2','fontsize',14,'fontweight','b')
 zlabel('Joint angle 3 q_3','fontsize',14,'fontweight','b')
-
+    
 %% Reconstruct path and animate it and/or create video
 vidoe = false;
 
 if(strcmp(rrt_variant, 'baseline'))
     % Pick random node on tree and move arm back to start position
     %q_goal = G(1:3, randi(length(G)));    
-    q_goal = G(1:3, end); 
+    q_last = G(1:3, end); 
 elseif(strcmp(rrt_variant, 'goal_directed') || strcmp(rrt_variant, 'goal_connect'))
     % Pick last node added to tree, i.e. goal configuration
-    q_goal = G(1:3, end);
+    q_last = G(1:3, end);
 end
 
-qSum = q_goal;
+qSum = q_last;
 
 % Reconstruct path taken
 i = length(G);
@@ -199,7 +260,7 @@ end
 if(strcmp(rrt_variant, 'baseline'))
     drawScene(file, qSum', ql, '', fk(q_init, ql), fk(q_goal, ql));
 elseif(strcmp(rrt_variant, 'goal_directed') || strcmp(rrt_variant, 'goal_connect'))
-    drawScene(file, qSum', ql, 'test', fk(q_init, ql), fk(q_goal, ql));
+    drawScene(file, qSum', ql, '', fk(q_init, ql), fk(q_goal, ql));
 end
 
 % %% Prepare tree for video - call createVideo(tree, 'filename', step)
